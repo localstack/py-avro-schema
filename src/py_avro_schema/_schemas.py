35c06fed58dd34e731a6cedc1f3a098bcf1b69a8
Giovanni Grano <me@giograno.com>
2025-11-11 19:02:39 +0100

    better error handling

--- src/py_avro_schema/_schemas.py
+++ src/py_avro_schema/_schemas.py
@@ -387,7 +387,10 @@ class FinalSchema(Schema):
         """An Avro schema for Python ``typing.Final``"""
         super().__init__(py_type, namespace, options)
         py_type = _type_from_annotated(py_type)
-        real_type = get_args(py_type)[0]
+        try:
+            real_type = get_args(py_type)[0]
+        except IndexError:
+            raise TypeError("Can't generate Avro schema from Python typing.Final without a type parameter")
         self.real_schema = _schema_obj(real_type, namespace=namespace, options=options)
 
     def data(self, names: NamesType) -> JSONType:
@@ -398,7 +401,7 @@ class FinalSchema(Schema):
     def handles_type(cls, py_type: Type) -> bool:
         """Whether this schema class can represent a given Python class"""
         py_type = _type_from_annotated(py_type)
-        return get_origin(py_type) is Final
+        return get_origin(py_type) is Final or py_type is Final
 
 
 @register_schema
