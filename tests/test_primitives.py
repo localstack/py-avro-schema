acbf7d08752342f7d72723a3591bc865ce897f3a
Benjamin Simon <benjh.simon@gmail.com>
2026-02-05 20:18:20 +0100

    fix Enum validation

--- tests/test_primitives.py
+++ tests/test_primitives.py
@@ -545,6 +545,16 @@ def test_str_enum_invalid_name():
     assert_schema(OriginProtocolPolicy, expected)
 
 
+def test_str_enum_invalid_name_with_dot():
+    class StateReasonCode(enum.StrEnum):
+        FunctionError_ExtensionInitError = "FunctionError.ExtensionInitError"
+        FunctionError_InvalidEntryPoint = "FunctionError.InvalidEntryPoint"
+
+    expected = {"namedString": "StateReasonCode", "type": "string"}
+
+    assert_schema(StateReasonCode, expected)
+
+
 def test_duplicated_invalid_enum():
     class OriginProtocolPolicy(str, enum.Enum):
         http_only = "http-only"
